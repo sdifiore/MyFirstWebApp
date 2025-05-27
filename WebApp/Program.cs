95f5af7b08c99672203ede8f4d84c9b312339b9a
Sergio Di Fiore <difiores@outlook.com>
2025-05-27 16:34:40 -0300

    httpcontext

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    await context.Response.WriteAsync($"The method is: {context.Request.Method}\r\n");
+    await context.Response.WriteAsync($"The Url is: {context.Request.Path}\r\n");
+    await context.Response.WriteAsync($"\r\n");
+
+    foreach (var key in context.Request.Headers.Keys)
+    {
+        string? Key = null;
+        await context.Response.WriteAsync($"{Key}: {context.Request.Headers[key]}\r\n");
+    }
+});
 
 app.Run();
